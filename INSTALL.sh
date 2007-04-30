f9c62f1195c7b297d822cd3ef0c289b205643770
Matteo Nastasi (mop) <nastasi@alternativeoutput.it>
2007-04-30 13:25:02 +0000

    cambiato file di config

--- INSTALL.sh
+++ INSTALL.sh
@@ -7,8 +7,8 @@ ftok_path="/var/lib/brisk"
 web_path="$HOME/brisk"
 web_only=0
 
-if [ -f $HOME/.brisk_install ]; then
-   . $HOME/.brisk_install
+if [ -f $HOME/.brisk06_install ]; then
+   . $HOME/.brisk06_install
 fi
 
 function usage () {
