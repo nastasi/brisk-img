0aa966b8096ad2ab148416ad43e851a22b9a5cd1
Matteo Nastasi <nastasi@alternativeoutput.it>
2020-05-03 21:08:21 +0200

    add second deck with slim cards

--- bin/sprite_main.sh
+++ bin/sprite_main.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-./bin/borderizer.sh briskin5/img briskin5/img/with_border_template.png ""
-./bin/spriter.sh briskin5/img "_bord" briskin5/img/cards_xx
+./bin/borderizer.sh briskin5/single_cards_xx briskin5/single_cards_xx/with_border_template.png ""
+./bin/spriter.sh briskin5/single_cards_xx "_bord" briskin5/img/cards_xx
+
+./bin/borderizer.sh briskin5/single_cards_yy briskin5/single_cards_yy/with_border_template.png ""
+./bin/spriter.sh briskin5/single_cards_yy "_bord" briskin5/img/cards_yy
