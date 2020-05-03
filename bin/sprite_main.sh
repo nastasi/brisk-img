a0a60175da199e687e19998d4d3fe35f82682d31
Matteo Nastasi <nastasi@alternativeoutput.it>
2020-05-03 09:19:44 +0200

    updates

--- bin/sprite_main.sh
+++ bin/sprite_main.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 ./bin/borderizer.sh briskin5/img briskin5/img/with_border_template.png ""
 ./bin/spriter.sh briskin5/img "_bord" briskin5/img/cards_xx
-
-# geeqie briskin5/img/cards_xx_bord.png
-
-
-# ./bin/borderizer.sh briskin5/img briskin5/img/with_border_template_side.png "_ea"
-# ./bin/spriter.sh briskin5/img "_bord_ea" briskin5/img/cards_xx
-# geeqie briskin5/img/cards_xx_bord_ea.png
-
-# ./bin/borderizer.sh briskin5/img briskin5/img/with_border_template_side.png "_we"
-# ./bin/spriter.sh briskin5/img "_bord_we" briskin5/img/cards_xx
-# geeqie briskin5/img/cards_xx_bord_we.png
--- bin/spriter.sh
+++ bin/spriter.sh
@@ -19,6 +19,13 @@ h=$(identify -format "%h" ${fold}/00${sfx}.png)
 convert -size $((w * 9))x$((h * 5)) xc:none ${fou}${sfx}_bg.png
 convert -size $((h * 9))x$((w * 5)) xc:none ${fou}${sfx}_oriz_bg.png
 
+convert -size ${w}x${h} xc:none ${fou}${sfx}_empty.png
+convert -size ${h}x${w} xc:none ${fou}${sfx}_empty_eawe.png
+
+cssname="${fou}${sfx}.css"
+rm -f $cssname
+touch $cssname
+
 for direction in "" "_ea" "_we"; do
     horiz_sfx=""
     if [ "$direction" != "" ]; then
@@ -36,6 +43,14 @@ for direction in "" "_ea" "_we"; do
                 y=$(((card % 5) * w))
             fi
 
+            card_id=$(printf "%02d" "$((seed * 10 + card))")
+            cat <<EOF >> $cssname
+img[data-card-id="${card_id}${direction}"] {
+    background: url('img/cards_xx${direction}.png') -${x}px -${y}px;
+}
+
+EOF
+            
             fin_no=$(printf "%s/%02d%s.png" "$fold" "$((seed * 10 + card))" "$sfx")
             fin=$(printf "%s/%02d%s%s.png" "$fold" "$((seed * 10 + card))" "$sfx" "$direction")
             if [ $seed -lt 4 ]; then
@@ -62,4 +77,4 @@ for direction in "" "_ea" "_we"; do
     echo "Created ${fou}${sfx}${direction}.png"
 done
 
-rm ${fou}${sfx}_bg.png ${fou}${sfx}_oriz_bg.png ${fold}/[0-3][0-9]${sfx}*.png
+rm  ${fold}/[0-3][0-9]${sfx}*.png ${fou}${sfx}_bg.png ${fou}${sfx}_oriz_bg.png
